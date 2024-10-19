f406dc6985c271a8e39ff35435319c90b1972884
Akshay Rajendran <akshayrajendran17499@gmail.com>
2024-10-19 05:59:36 +0530

    PERMUTATION ALGORITHM

--- Steinhaus-Johnson-Trotter Algorithm.c
+++ Steinhaus-Johnson-Trotter Algorithm.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
-
-void print(int *permu,int n)
+int arr[50];
+int z=0;
+void  print(int*permu,int n)
 {
+    int num=0;
     for(int i=0;i<n;i++)
     {
-        printf("%d",permu[i]);
+       int digit=permu[i];
+       num=num*10+digit;
+        
     }
-    printf("\n");
+    arr[z++]=num;
+   // printf("%d",num);
+    //printf("\n");
 }
-
-int largest_mobile_index(int*permu,int*dir,int n)
+int largest_mobile(int*permu,int *dir,int n)
 {
     int largestindex=-1;
     for(int i=0;i<n;i++)
@@ -28,35 +33,32 @@ int largest_mobile_index(int*permu,int*dir,int n)
     return largestindex;
 }
 
-void swap(int *temp1,int *temp2)
+void swap(int*temp1,int*temp2)
 {
     int temp=*temp1;
     *temp1=*temp2;
     *temp2=temp;
 }
 
-void sjt_algo(int n)
+void sjt_algo(int*permu,int n)
 {
-    int permu[n];
     int dir[n];
-
     for(int i=0;i<n;i++)
     {
-        permu[i]=i+1;
         dir[i]=0;
     }
-
+    int count=0;
     int largestindex;
     print(permu,n);
-
+    count++;
     while(1)
     {
-        largestindex=largest_mobile_index(permu,dir,n);
-
+        largestindex=largest_mobile(permu,dir,n);
+        
         if(largestindex==-1)
         break;
-
-        if(dir[largestindex]==0&&permu[largestindex]>permu[largestindex-1])
+        
+        if(dir[largestindex]==0)
         {
             swap(&permu[largestindex],&permu[largestindex-1]);
              swap(&dir[largestindex],&dir[largestindex-1]);
@@ -64,11 +66,11 @@ void sjt_algo(int n)
         }
         else
         {
-            swap(&permu[largestindex],&permu[largestindex+1]);
-            swap(&dir[largestindex],&dir[largestindex+1]);
-             largestindex++;
+          swap(&permu[largestindex],&permu[largestindex+1]);
+          swap(&dir[largestindex],&dir[largestindex+1]);
+          largestindex++; 
         }
-
+        
         for(int i=0;i<n;i++)
         {
             if(permu[i]>permu[largestindex])
@@ -76,14 +78,40 @@ void sjt_algo(int n)
                 dir[i]=1-dir[i];
             }
         }
+        
         print(permu,n);
+        count++;
     }
+   printf("Total no of permutation possible on given n.o are : %d\n",count); 
 }
 
+void bubble(int*permu,int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(permu[j]>permu[j+1])
+            swap(&permu[j],&permu[j+1]);
+        }
+    }
+}
 int main()
 {
-    int num;
-    printf("Enter number to find permutation from 1 to that num : ");
-    scanf("%d",&num);
-    sjt_algo(num);
+    int n;
+    printf("enter size of array : ");
+    scanf("%d",&n);
+    int permu[n];
+    printf("enter array elements : ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&permu[i]);
+    }
+    bubble(permu,n);
+    sjt_algo(permu,n);
+    bubble(arr,z);
+   for(int i=0;i<z;i++)
+    {
+        printf("%d\n",arr[i]);
+    } 
 }
